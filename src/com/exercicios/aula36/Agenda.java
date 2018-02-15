f3f7a818b81e46e7d9684f7e92815659cce66e81
Benjamin <tiago.rs.andrade@hotmail.com>
2018-02-15 07:47:10 -0200

    classes em classes exercicios

---
+++ src/com/exercicios/aula36/Agenda.java
@@ -0,0 +1,20 @@
+package com.exercicios.aula36;
+
+public class Agenda {
+	private String nome;
+	private Contato contato;
+	
+	public Contato getContato() {
+		return contato;
+	}
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
---
+++ src/com/exercicios/aula36/Ex00.java
@@ -0,0 +1,10 @@
+package com.exercicios.aula36;
+
+public class Ex00 {
+
+	public static void main(String[] args) {
+		Contato c1 = new Contato();
+		c1.setNome("tiago");
+	}
+
+}
